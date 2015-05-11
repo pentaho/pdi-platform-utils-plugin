375ab03cec0ab7e5e36ceb4941a25ca5a27f25d8
Diogo Mariano <diogo.mariano@webdetails.pt>
2015-05-11 16:10:12 +0100

    [BACKLOG-3346] - Updated project folder name and settings.xml reference

--- src/org/pentaho/di/baserver/utils/SetSessionVariableData.java
+++ src/org/pentaho/di/baserver/utils/SetSessionVariableData.java
@@ -39,7 +39,7 @@ public class SetSessionVariableData extends BaseStepData implements StepDataInte
   protected void setBlackList() {
     try {
       if ( Class.forName( "org.pentaho.platform.engine.core.system.PentahoSystem" ) != null ) {
-        String variablesBlackList = PentahoSystem.getSystemSetting( "kettle/plugins/BAServerUtils/settings.xml",
+        String variablesBlackList = PentahoSystem.getSystemSetting( "kettle/plugins/platform-utils-plugin/settings.xml",
             "session-variables-blacklist", "" );
         blackList = Arrays.asList( variablesBlackList.split( "," ) );
       }
